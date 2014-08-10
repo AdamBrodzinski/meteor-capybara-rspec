07f6fb498a99b7a25cd0a536d7b409eda52077a8
Adam Brodzinski <adambrodzinski@gmail.com>
2014-08-10 19:19:27 -0400

    adds more helpers

--- spec/support/general_helpers.rb
+++ spec/support/general_helpers.rb
@@ -13,11 +13,36 @@ module GeneralTestHelpers
     evaluate_script(statement)
   end    
 
+  def root_path
+    '/'
+  end       
+
 end
 
 #and the following config allows us to simply call the helpers
 #from within our RSpec examples
 RSpec.configure do |config|
   config.include GeneralTestHelpers
+end
+
+# allow user to use a JavaScript like notation for accessing object from the
+# RunJS return value. 
+#
+# Example: 
+#   foo = RunJS("{bar: 'baz'})
+#   foo.bar == 'baz'
+#   # or without helper... 
+#   foo['bar'] == 'baz'
+#
+class Hash
+  def method_missing(method, *opts)
+    m = method.to_s
+    if self.has_key?(m)
+      return self[m]
+    elsif self.has_key?(m.to_sym)
+      return self[m.to_sym]
+    end
+    super
+  end
 end
 
--- spec/support/meteor_helper.rb
+++ spec/support/meteor_helper.rb
@@ -1,4 +1,8 @@
 module MeteorHelpers
+  # return the Meteor user id
+  def user_id
+    evaluate_script("Meteor.userId();")
+  end  
 
   # logout Meteor user if logged in
   def logout_user
@@ -15,7 +19,19 @@ module MeteorHelpers
     evaluate_script("Router.go('#{route_name}')")
   end     
 
+  # login user using Accounts UI (not tested with Bootstrap)
+  def login_user
+    logout_user() #make sure we're not already logged in from prev test
+
+    find('#login-sign-in-link').click
+
+    fill_in "Email", with: "foo@bar.com"
+    fill_in "Password", with: "password"
+
+    find('#login-buttons-password').click
+  end             
 end
+
 
 #following config allows us to simply call the helpers from within our examples
 RSpec.configure do |config|
